6c7bfdfafe03e202b51bc718fc4e09fcea2511ce
Zeki Burak Özden <zburakozden@gmail.com>
2024-03-25 23:30:08 +0300

    silme, duzenleme ve id ye gore goruntuleme islemleri

--- haberbulteni/urls.py
+++ haberbulteni/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('haberler.urls')),
+    path('api/', include('haberler.api.urls')),
 ]
